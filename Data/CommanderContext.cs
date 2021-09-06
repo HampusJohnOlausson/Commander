e8c1dbe601827fbb533e5fdaeba4b2455851fdf4
hampus <hampus.john.olausson@gmail.com>
2021-09-06 19:41:07 +0200

    added dbcontext and packages

---
+++ Data/CommanderContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Commander.Data
+{
+    public class CommanderContext : DbContext
+    {
+        public CommanderContext(DbContextOptions<CommanderContext> opt) : base(opt)
+        {
+            
+        }
+    }
+}
